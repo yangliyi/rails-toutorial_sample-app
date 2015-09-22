70cb9e3c910f93a5ce5822b3ea0510911a760cb6
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-09-22 15:59:29 +0800

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
 
+  root 'static_pages#home'
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
